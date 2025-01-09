f85983abe82db717d19386be9ec5a34b0f7f42fa
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2025-01-09 15:03:58 -0600

    Usando host en otros componentes

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
+  host: {
+    id: 'server-status',
+  },
 })
 export class ServerStatusComponent {
   currentStatus = 'online';

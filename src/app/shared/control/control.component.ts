001aa25511e890936c0feea1c419e095a13cbaf7
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2025-01-08 19:02:33 -0600

    Usando host

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -6,8 +6,12 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  //Esta configuración
+  //Esta configuración sirve para que se amplie el escope de los estiolos
   encapsulation: ViewEncapsulation.None,
+  //Añadira como propiedades todo lo que este acá pero solo a el app-control
+  host: {
+    class: 'control',
+  },
 })
 export class ControlComponent {
   @Input({ required: true }) labelText!: string;

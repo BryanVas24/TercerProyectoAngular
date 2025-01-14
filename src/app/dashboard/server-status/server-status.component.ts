8c6620b3a07d2126e7935847bb63b02f13dbd4d5
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2025-01-14 13:25:20 -0600

    Usando de manera sencilla el ngOndestroy

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -10,13 +10,18 @@ import { Component } from '@angular/core';
     id: 'server-status',
   },
 })
-export class ServerStatusComponent {
+//implements es de ts y busca una interfaz para que la clase tenga cierta forma
+export class ServerStatusComponent implements OnInit, OnDestroy {
   currentStatus = 'online';
-
+  private interval?: ReturnType<typeof setInterval>;
   ngOnInit() {
     // Simulate server status fetching
-    setInterval(() => {
+    this.interval = setInterval(() => {
       this.currentStatus = Math.random() > 0.5 ? 'online' : 'offline';
     }, 5000);
+  }
+  //El orden de los hooks acá no importa
+  ngOnDestroy(): void {
+    clearTimeout(this.interval);
   }
 }

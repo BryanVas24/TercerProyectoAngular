0237acdbcf3c6f86d36af3b88918cc13cea343dc
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2025-01-14 17:37:00 -0600

    usando variables de plantilla para tomar datos de un form

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -11,16 +11,17 @@ import { Component, DestroyRef, inject, OnInit } from '@angular/core';
   },
 })
 //implements es de ts y busca una interfaz para que la clase tenga cierta forma
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit, OnDestroy {
   currentStatus = 'online';
-  private destroyRef = inject(DestroyRef);
+  private interval?: ReturnType<typeof setInterval>;
   ngOnInit() {
     // Simulate server status fetching
-    const interval = setInterval(() => {
+    this.interval = setInterval(() => {
       this.currentStatus = Math.random() > 0.5 ? 'online' : 'offline';
     }, 5000);
-    this.destroyRef.onDestroy(() => {
-      clearInterval(interval);
-    });
+  }
+  //El orden de los hooks acá no importa
+  ngOnDestroy(): void {
+    clearTimeout(this.interval);
   }
 }

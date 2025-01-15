e0a33aa7415192e9fa31fbedddad609eb055beae
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2025-01-15 15:59:27 -0600

    Añadiendo tickets al array

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -10,12 +17,20 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {
+//Lo del implements es más que todo para prevenir  errores de tipo
+export class NewTicketComponent implements AfterViewInit {
+  //Así se recibe una variable de plantilla (El elementref envuelve un tipo)
   @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  @Output() add = new EventEmitter<{ title: string; text: string }>();
 
+  //private form= viewChild.required <ElementRef<HTMLFormElement>>("form");
   onSubmit(title: string, message: string) {
-    console.log(`New Ticket: ${title} - ${message}`);
+    this.add.emit({ title, text: message });
     //El native element es porque el tipo del form esta envuelto por el ElementRef
     this.form?.nativeElement.reset();
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.form?.nativeElement);
   }
 }
